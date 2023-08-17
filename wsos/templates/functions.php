48fb9dfa9058a40518bb01a17b2f26a8d255a46f
Lukáš Plevač <lukas@plevac.eu>
2023-08-17 14:40:54 +0200

    Added suppoer for FOREACH and IF template functions

--- wsos/templates/functions.php
+++ wsos/templates/functions.php
@@ -52,6 +52,16 @@
                 "db" => [
                     "fnc"    => '\wsos\templates\functions::db',
                     "params" => 2
+                ],
+
+                "foreach" => [
+                    "fnc"    => '\wsos\templates\functions::foreachf',
+                    "params" => 3
+                ],
+
+                "if" => [
+                    "fnc"    => '\wsos\templates\functions::iff',
+                    "params" => 3
                 ]
             ];
         }
@@ -70,11 +80,7 @@
         }
 
         public static function bind($params, $context) {
-            if (!array_key_exists($params[0], $context->binding)) {
-                return "Fail to bind {$params[0]}";
-            }
-    
-            return $context->binding[$params[0]];
+            return $context->getBinding($params[0]);
         }
 
         public static function round($params, $context) {    
@@ -118,5 +124,65 @@
 
             return $db->table($table)->find($colSel, $colSelVal)[$colRead];
         } 
+
+        public static function foreachf($params, $context) {
+            $array  = $context->getBinding($params[0]);
+            $loader = $context->loader->newInDir();
+            $html   = "";
+
+            if ($array instanceof \wsos\database\core\table) {
+                $array = $array->getAll()->values;
+            }
+
+            foreach ($array as $item) {
+                $binding         = $context->binding;
+                $binding["item"] = $item; 
+
+                if ($params[1] == "RENDER") {
+                    $loader->load($params[2]);
+                } else if ($params[1] == "USE") {
+                    $loader->loadHtml($params[2]);
+                }
+
+                $loader->render($binding);
+                $html .= $loader->getHtml();
+            }
+
+            return $html;
+        }
+
+        public static function iff($params, $context) {
+            $condition = $params[0];
+            $operands  = explode(" ", str_replace(['==', '!=', '<=', '>=', '<', '>'], " ", $condition));
+            preg_match_all('/[=,<,>,!]+/i', $condition, $operators);
+
+            $operators = $operators[0];
+
+            $res = true;
+            for ($i = 0; $i < count($operands) - 1; $i++) {
+                if ($operators[$i] == '==')      $res = $res && ($context->getBinding($operands[$i]) == $context->getBinding($operands[$i + 1]));
+                elseif ($operators[$i] == '!=')  $res = $res && ($context->getBinding($operands[$i]) != $context->getBinding($operands[$i + 1]));
+                elseif ($operators[$i] == '<=')  $res = $res && ($context->getBinding($operands[$i]) <= $context->getBinding($operands[$i + 1]));
+                elseif ($operators[$i] == '>=')  $res = $res && ($context->getBinding($operands[$i]) >= $context->getBinding($operands[$i + 1]));
+                elseif ($operators[$i] == '<')   $res = $res && ($context->getBinding($operands[$i]) <  $context->getBinding($operands[$i + 1]));
+                elseif ($operators[$i] == '>')   $res = $res && ($context->getBinding($operands[$i]) >  $context->getBinding($operands[$i + 1]));
+                else $res = false;
+            }
+
+            if (!$res) return "";
+
+            $loader = $context->loader->newInDir();
+            $html   = "";
+
+            if ($params[1] == "RENDER") {
+                $loader->load($params[2]);
+            } else if ($params[1] == "USE") {
+                $loader->loadHtml($params[2]);
+            }
+
+            $loader->render($context->binding);
+
+            return $loader->getHtml();
+        }
     }
 ?>
